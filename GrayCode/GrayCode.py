f47eb54c81ff632e127a11a18f3f98b78b8f5979
Sbbq <yxzzkx@163.com>
2016-05-10 15:44:09 +0800

    Create GrayCode.py

---
+++ GrayCode/GrayCode.py
@@ -0,0 +1,11 @@
+#binary2Gray  (num>>1)^num=graycode
+#Gray2binary  
+unsigned int grayToBinary(unsigned int num)
+{
+    unsigned int mask;
+    for (mask = num >> 1; mask != 0; mask = mask >> 1)
+    {
+        num = num ^ mask;
+    }
+    return num;
+}
