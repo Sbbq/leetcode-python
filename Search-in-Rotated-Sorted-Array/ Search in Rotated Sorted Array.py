457ae17e0af5657891ff46fc899f60b60285741d
Sbbq <yxzzkx@163.com>
2016-06-28 15:20:32 +0800

    Create  Search in Rotated Sorted Array.py

---
+++ Search-in-Rotated-Sorted-Array/ Search in Rotated Sorted Array.py
@@ -0,0 +1,28 @@
+#核心思想，先分块，找到递增的那部分，判断是否在递增部分里面，然后缩小范围
+class Solution:
+    # @param {integer[]} numss
+    # @param {integer} target
+    # @return {integer}
+    def search(self, nums, target):
+        if not nums:
+            return -1
+
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = (low + high) / 2
+            if target == nums[mid]:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+        return -1
