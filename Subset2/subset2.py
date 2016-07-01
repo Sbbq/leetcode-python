ca3436d426138e6b4c6c6ddbd9589485267b254a
Sbbq <yxzzkx@163.com>
2016-07-01 14:16:53 +0800

    Update subset2.py

--- Subset2/subset2.py
+++ Subset2/subset2.py
@@ -15,3 +15,24 @@ class Solution(object):
                 res+=[x+[nums[i]] for x in res ]
             previsize=size
         return res
+#dfs
+class Solution2(object):
+    def subsetsWithDup(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        res=[]
+        path=[]
+        nums.sort()
+        #should sort first
+        def dfs(nums,path,res,i):
+            res+=path,
+            for j in range(i,len(nums)):
+                if j>i and nums[j]==nums[j-1]:
+                #j>i not j>0
+                    continue
+                else:
+                    dfs(nums,path+[nums[j]],res,j+1)
+        dfs(nums,path,res,0)
+        return res
